73793ba60bcd7801a95ade04868c281a0ec7c1d1
Denis Geschwandtner <denis.geschwandtner@suportis.com>
2023-07-12 15:13:04 +0200

    Aufgabe 2.1 - Anonyme Funktionen mit einer Variable

---
+++ aufgaben_2,1.js
@@ -0,0 +1,17 @@
+/*
+let x = function(){
+    console.log("Anonyme Funktion mit einer Variablen");
+}
+
+
+
+function f2(y){
+    y();
+}
+
+f2(x);
+*/
+
+f2 ( function() { console.log("Anynome Funktion ohne Variable"); });
+
+
