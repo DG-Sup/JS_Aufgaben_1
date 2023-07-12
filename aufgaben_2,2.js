1cfb1e1376ef27911e66cfc56cddb04219b73cd4
Denis Geschwandtner <denis.geschwandtner@suportis.com>
2023-07-12 16:07:06 +0200

    Aufgabe 2.2 - 2 Funktionsaufrufe mit Parametern

--- aufgaben_2,2.js
+++ aufgaben_2,2.js
@@ -1,11 +1,18 @@
-function f2(f){
+function f2(c, d, f){
     // f ist der Parameter bzgl. der Deklaration der Funktion
-    f(2, 2);
+    f(c, d);
 }
 
-f2( function(a, b){ 
+
+
+
+f2(5, 10, function(a, b){ 
     // Aufruf der Funktion: Argumente für den Parameter f
     let x = a + b;
     console.log(`Die Summe beträgt: ${x}`);
 });
 
+f2(5, 10, function(a, b){
+    let x = a* b;
+    console.log(`Das Produkt beträgt: ${x}`);
+})
